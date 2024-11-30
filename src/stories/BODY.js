6a91adacf88ae11e2401cdf7a8643ba8310720eb
Trương Hoài Linh <131947376+TruongHoaiLinh3103@users.noreply.github.com>
2024-11-30 22:13:08 +0700

    Update BODY.js

--- src/stories/BODY.js
+++ src/stories/BODY.js
@@ -2,241 +2,241 @@ export const BODY = [
     {
         id: 0,
         name: "Đấm Móc",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 1,
         name: "Đấm Thẳng",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 2,
         name: "Đấm Giữa",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 3,
         name: "Đấm Ngược",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 4,
         name: "Khóa Tay",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 5,
         name: "Lên Chỏ",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 6,
         name: "Chỏ Ngang",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 7,
         name: "Chỏ Ngược",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 8,
         name: "Chỏ Trên",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 9,
         name: "Thủ Vạn Tay",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 10,
         name: "Đạp",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 11,
         name: "Đá",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 12,
         name: "Đá Xoay",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 13,
         name: "Đạp Xoay",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 14,
         name: "Đá Ngược",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 15,
         name: "Khóa Chân",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 16,
         name: "Lên đùi",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 17,
         name: "Ngang đùi",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 18,
         name: "Chạy đạp",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 19,
         name: "Đạp trước sau 2 chân",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 20,
         name: "Nhảy đạp 2 chân",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 21,
         name: "Nhảy đá 2 chân",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 22,
         name: "Nhảy đá ngược",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 23,
         name: "Nhảy đá xoay",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 24,
         name: "Nhảy đá xoay 2 lần",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 25,
         name: "Nhảy đạp",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 26,
         name: "Nhảy lên đùi",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 27,
         name: "Nhảy lên đùi ngang",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 28,
         name: "Nhảy chỏ chân",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 29,
         name: "Nhảy đạp tường xoay",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 30,
         name: "Nhảy bắp tay",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 31,
         name: "Chạy nhảy chỏ ngang",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 32,
         name: "Chạy nhảy đấm móc",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 33,
         name: "Chạy nhảy bổ cùi",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 34,
         name: "Chạy nhảy kẹp cổ bằng 2 chân",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 35,
         name: "Nhảy né",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 36,
         name: "Trái né",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 37,
         name: "Phải né",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 38,
         name: "Lui né",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 39,
         name: "Khom người né",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 40,
         name: "Kẹp tay bằng 2 chân",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 41,
         name: "Kẹp cổ bằng chỏ tay",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 42,
         name: "Vật người bằng lưng",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 43,
         name: "Vật người chi trái(phải)",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 44,
         name: "Nằm nhảy dậy",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 45,
         name: "Kẹp cổ bằng 2 chân",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 46,
         name: "Đỡ đòn bằng cù chỏ",
-        quantity: "100 cái"
+        quantity: "50 cái"
     },
     {
         id: 47,
         name: "Đá móc xuống",
-        quantity: "100 cái"
+        quantity: "50 cái"
     }
 ]
